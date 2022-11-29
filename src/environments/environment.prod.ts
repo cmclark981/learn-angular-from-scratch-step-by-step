c2a2b60522a8140701fe227c18294fd598bbfb57
Christopher Clark <cmclark981@gmail.com>
2022-11-29 16:35:34 -0600

    Module 6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  api_url: 'https://0dwrwz5e13.execute-api.us-east-1.amazonaws.com'
 };
